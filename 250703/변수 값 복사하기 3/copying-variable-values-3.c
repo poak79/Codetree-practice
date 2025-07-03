3b35a95569ece8e9a958cec6f212c27177b3f581
poak79 <parkyunseo5@gmail.com>
2025-07-03 17:24:22 +0900

    Title: 변수 값 복사하기 3, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250703/변수 값 복사하기 3/copying-variable-values-3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a = 1;
+    int b = 5;
+    int c = 3;
+
+    a = c;
+    a = a+c;
+    b = b-c;
+
+    printf("%d\n", a);
+    printf("%d\n", b);
+    printf("%d", c);
+    return 0;
+}
