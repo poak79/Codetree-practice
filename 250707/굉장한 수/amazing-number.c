6b060025147623f39d463a18708b1634894ca369
poak79 <parkyunseo5@gmail.com>
2025-07-07 18:48:07 +0900

    Title: 굉장한 수, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250707/굉장한 수/amazing-number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+
+    if(a%2==1 && a%3==0) {
+        printf("true");
+    }
+    else if(a%2==0 && a%5==0) {
+        printf("true");
+    }
+    else {
+        printf("false");
+    }
+    return 0;
+}
