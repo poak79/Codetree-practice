98ad90b3d9343235d4ada274c0b8e7b67fc1b1bf
poak79 <parkyunseo5@gmail.com>
2025-08-04 14:27:37 +0900

    Title: 1부터의 합, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250804/1부터의 합/sum-from-the-one.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int s=0;
+    int n;
+    scanf("%d", &n);
+
+    for(int i=1;i<=100;i++) {
+        s+=i;
+        if(s>=n) {
+            printf("%d", s-i);
+            break;
+        }
+        
+    }
+    return 0;
+}
