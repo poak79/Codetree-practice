b6bf7e44b1f453e6806790403db9dfb29abedb08
poak79 <parkyunseo5@gmail.com>
2025-09-20 22:27:53 +0900

    Title: 최대공약수 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250920/최대공약수 구하기/find-the-greatest-common-divisor.c
+++ 250920/최대공약수 구하기/find-the-greatest-common-divisor.c
@@ -4,7 +4,7 @@ int print(int a, int b) {
     int num=0;
     int smaller=(a<b) ? a : b;
 
-    for(int i=1; i<smaller; i++) {
+    for(int i=1; i<=smaller; i++) {
         if(a%i==0 && b%i==0) {
             num=i;
         }
